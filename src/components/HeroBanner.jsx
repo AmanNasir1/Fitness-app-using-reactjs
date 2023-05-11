a87840813c7cd3cbf0851b86359632aa2cc96854
Aman Nasir <aman.nasir231@gmail.com>
2023-05-12 01:23:44 +0500

    navbar added

---
+++ src/components/HeroBanner.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Box, Stack, Typography } from "@mui/material";
+
+const HeroBanner = () => {
+  return (
+    <Box>
+      <Typography color="#FF">Fitness Club</Typography>
+    </Box>
+  );
+};
+
+export default HeroBanner;
