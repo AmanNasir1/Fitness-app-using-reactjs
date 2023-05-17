e67049895603f4434a56cf6df63fc9e5707642ee
Aman Nasir <aman.nasir231@gmail.com>
2023-05-17 16:07:40 +0500

    search filter added

---
+++ src/utils/fetchData.js
@@ -0,0 +1,13 @@
+export const exerciseOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+    "X-RapidAPI-Key": "eca15c23ddmsh50de9e7c725d708p1b56bejsn962a6d2a8315",
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const response = await fetch(url, options);
+  const data = await response.json();
+  return data;
+};
